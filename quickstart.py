abdf290cb57e470bf2822b75136b2b9f41fc8914
null-dreams <agneeva0705@gmail.com>
2026-03-02 14:42:25 +0530

    modify(api): modularize data fetching with error handling

--- quickstart.py
+++ quickstart.py
@@ -8,6 +8,31 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
+def get_body(payload):
+    if payload.get("body", {}).get("data"):
+        return payload["body"]["data"]
+    for part in payload.get("parts", []):
+        if part["mimeType"] in ["text/plain", "text/html"]:
+            return part.get("body", {}).get("data")
+        elif part["mimeType"].startswith("multipart/"):
+            body = get_body(part)
+            if body:
+                return body
+
+def get_header_data(headers):
+    sep = " "
+    for header in headers:
+        if header["name"] == "Date":
+            header_date = header["value"].split(' ')
+            date = sep.join(header_date[1:4])
+            time = header_date[-2]
+        if header["name"] == "From":
+            sender_data = header["value"].split(' ')
+            sender_name = sep.join(sender_data[:-1])
+            sender_email = sender_data[-1][1:-1]
+
+    print(f"Sender Name: {sender_name}\nSender Email: {sender_email}\nData: {date}\nTime: {time}")
+
 def main():
     creds = None
     if os.path.exists("token.json"):
@@ -25,24 +50,23 @@ def main():
 
     try:
         service = build("gmail", "v1", credentials=creds)
-        results = service.users().messages().list(userId="me", maxResults=2).execute()
-        mail_id = results["messages"][0]["id"]
+        results = service.users().messages().list(userId="me").execute()
+        mail_id = results["messages"][1]["id"]
         message = service.users().messages().get(userId="me", id=mail_id, format="full").execute()
 
         payload = message["payload"]
-        parts = payload.get("parts", [])
+        get_header_data(payload["headers"])
+
         body_data = None
+        body_data = get_body(payload=payload)
+        if body_data is not None:
+            decoded_body = base64.urlsafe_b64decode(body_data).decode("utf-8")
+            print(decoded_body)
 
-        if parts:
-            for part in parts:
-                if part["mimeType"] == "text/plain":
-                    body_data = part["body"]["data"]
-                    break
-        else:
-            body_data = payload["body"].get("data")
+            
+            with open("temp.txt", "w") as f:
+                f.write(decoded_body)
 
-        decoded_body = base64.urlsafe_b64decode(body_data).decode("utf-8")
-        print(decoded_body)
     except HttpError as error:
         print(f"An error occured: {error}")
     
